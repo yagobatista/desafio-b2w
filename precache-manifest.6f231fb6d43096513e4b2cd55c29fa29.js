c9c454110e065ace80d3355fe56a14cefb12d75b
yago batista da silva <yagobatistasilva@gmail.com>
2019-12-16 01:02:43 -0200

    Updates

---
+++ precache-manifest.6f231fb6d43096513e4b2cd55c29fa29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8ae4ad6980e923263bf861f7968a59",
+    "url": "/desafio-b2w/index.html"
+  },
+  {
+    "revision": "b6a2b1d3c4db52c0abc4",
+    "url": "/desafio-b2w/static/css/main.42ff2ebc.chunk.css"
+  },
+  {
+    "revision": "1cecacb0eb920cccb8ef",
+    "url": "/desafio-b2w/static/js/2.bff6bba5.chunk.js"
+  },
+  {
+    "revision": "e3f93008bf1ade31a548596943d7cdf8",
+    "url": "/desafio-b2w/static/js/2.bff6bba5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6a2b1d3c4db52c0abc4",
+    "url": "/desafio-b2w/static/js/main.be6dde5d.chunk.js"
+  },
+  {
+    "revision": "d06cb5c980b8b32022c3",
+    "url": "/desafio-b2w/static/js/runtime-main.3e02fa0b.js"
+  },
+  {
+    "revision": "9ce80ecb034b7194819f84b2fa25f78e",
+    "url": "/desafio-b2w/static/media/background.9ce80ecb.jpg"
+  }
+]);
